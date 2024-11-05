4b12f15abe9044acdcccf91f5e81c4146f16cbb1
rangguy <ranggadwi100@gmail.com>
2024-11-05 16:10:29 +0700

    Auto Increment

--- sql_test.go
+++ sql_test.go
@@ -169,3 +169,25 @@ func TestExecSqlParameter(t *testing.T) {
 
 	fmt.Println("Success insert new user")
 }
+
+func TestAutoIncrement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	email := "test@mail.com"
+	comment := "autoincrement"
+
+	sqlStmnt := "INSERT INTO comments (email, comment) VALUES (?, ?)"
+
+	result, err := db.ExecContext(ctx, sqlStmnt, email, comment)
+	if err != nil {
+		panic(err)
+	}
+	insertId, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Last Insert id:", insertId)
+}
