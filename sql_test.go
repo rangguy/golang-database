ba2bb605b66ff511ef74220e99454fcbee204155
rangguy <ranggadwi100@gmail.com>
2024-11-05 19:38:07 +0700

    Database Transaction

--- sql_test.go
+++ sql_test.go
@@ -220,3 +220,36 @@ func TestPrepareStatement(t *testing.T) {
 		fmt.Println("Comment id:", id)
 	}
 }
+
+func TestDatabaseTransaction(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	sqlStmt := "INSERT INTO comments (email, comment) VALUES (?, ?)"
+	// do transaction
+	for i := 0; i < 10; i++ {
+		email := "rangga" + strconv.Itoa(i) + "@mail.com"
+		comment := "Komentar ke " + strconv.Itoa(i)
+		result, err := tx.ExecContext(ctx, sqlStmt, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Comment id:", id)
+	}
+
+	err = tx.Rollback()
+	if err != nil {
+		panic(err)
+	}
+}
